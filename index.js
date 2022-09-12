40742269ac410feaf827d7cbf9ebf79df369c951
ELASHMAWYDEV <elashmawydev@gmail.com>
2022-09-12 18:31:20 +0200

    fixed app listening to port

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ const cookieParser = require("cookie-parser");
 const paypal = require("paypal-rest-sdk");
 const PORT = process.env.PORT || 5002;
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 //Init firebase
 admin.initializeApp();
 
-
 //Init Paypal
 paypal.configure({
-	mode: (process.env.PAYPAL_SANDBOX_ACTIVE || functions.config().paypal.is_sandbox_active) ? "sandbox" : "live",
-	client_id: process.env.PAYPAL_CLIENTID || functions.config().paypal.client_id,
-	client_secret: process.env.PAYPAL_SECRET || functions.config().paypal.secret,
+  mode: process.env.PAYPAL_SANDBOX_ACTIVE || functions.config().paypal.is_sandbox_active ? "sandbox" : "live",
+  client_id: process.env.PAYPAL_CLIENTID || functions.config().paypal.client_id,
+  client_secret: process.env.PAYPAL_SECRET || functions.config().paypal.secret,
 });
 
 //Middlewares
@@ -32,7 +31,7 @@ app.use("/api", require("./api"));
 
 /*********************************************************/
 
-if (process.env.NODE_ENV != "production")
-	app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+if (process.env.NODE_ENV != "production" || process.env.IS_HEROKU)
+  app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
 module.exports.app = functions.https.onRequest(app);
